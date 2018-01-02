b4f6b04a270b9524b453acb863e0d1c61cd84b81
Ralph Dworzanski <a1302228@unet.univie.ac.at>
2018-01-02 18:41:52 +0100

    Added logging base feature

--- src/main/java/Game/GameController.java
+++ src/main/java/Game/GameController.java
@@ -1,8 +1,12 @@
 package Game;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GameController {
 	private GameModel model;
 	private GameView view;
+	private Logger logger = LoggerFactory.getLogger(GameController.class);
 	
 	public GameController() {
 		model = new GameModel();
