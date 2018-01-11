55f674ca0c95a84de4e233d11c0a1f954355fa7b
Ralph Dworzanski <a1302228@unet.univie.ac.at>
2018-01-11 18:22:40 +0100

    Fixed bug in map generation, adhering all business rules. Fixed storing other map half

--- src/main/java/game/Map.java
+++ src/main/java/game/Map.java
@@ -1,6 +1,5 @@
 package game;
 
-import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.slf4j.Logger;
@@ -19,35 +18,35 @@ public class Map {
 	}
 	
 	public boolean generateOwnHalf() {
-		int grassCount = 0, waterCount = 0, mountainCount = 0;
+		int grassCount = 0, waterCount = 0, mountainCount = 0, nodeCount = 0;
 		int randNum;
 		MapNode node = null;
 		for(int i = 0; i < 4; i++) {
-			System.out.println("I:" + i);
 			for(int j = 0; j < 8; j++) {
-				System.out.println("J:" + j);
-				randNum = 4; //ThreadLocalRandom.current().nextInt(0, 11);
-				if(randNum < 2 && waterCount != 4) {
+				randNum = ThreadLocalRandom.current().nextInt(0, 11);
+				if(randNum < 2 && waterCount != 4 && i != 0) {
 					node = new MapNode("water", false, false);
 					//System.out.println("Rand is "+ randNum + ": Water");
 					++waterCount;
 					
-				} else if (randNum >= 2 && randNum < 6) {
-					node = new MapNode("grass", false, false);
-					//System.out.println("Rand is "+ randNum + ": Grass");
-					++grassCount;
-					
-				} else if (randNum > 6 && mountainCount != 3) {
+				} else if (randNum > 6 && mountainCount != 4) {
 					node = new MapNode("mountain", false, false);
 					//System.out.println("Rand is "+ randNum + ": Mountain");
 					++mountainCount;
 					
+				} else {
+					node = new MapNode("grass", false, false);
+					//System.out.println("Rand is "+ randNum + ": Grass");
+					++grassCount;
 				}
 				_ownHalf[i][j] = node;
-				
+				nodeCount++;
 			}
-			
 		}
+		
+		System.out.println("I generated " + waterCount + " water Tiles");
+		System.out.println("I generated " + mountainCount + " mountain Tiles");
+		System.out.println("I generated " + grassCount + " grass Tiles");
 		
 		/*This is to uphold business rules. Map generation happens quickly and is
 		 * fairly cheap. If this function returns false, we'll just generate a new map. By
@@ -61,11 +60,13 @@ public class Map {
 		return true;
 	}
 	
-	public void setOtherHalf(ArrayList<MapNode> otherMapHalf) {
+	public void setOtherHalf(MapNode[][] otherMapHalf) {
 		try {
-			for(MapNode node : otherMapHalf) {
-				otherMapHalf.add(node);
-			} 
+			for(int i = 0; i < 4; i++) {
+				for(int j = 0; j < 8; j++) {
+					_otherHalf[i][j] = otherMapHalf[i][j];
+				}
+			}
 		}
 		catch (NullPointerException npe) {
 				npe.printStackTrace();
